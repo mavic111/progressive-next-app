842c5003cd4c220fec2fa77b6e937e45c9e47dc5
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-06-18 08:08:44 +0700

    Modularize heroicons import

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  modularizeImports: {
+    '@heroicons/react/24/outline/?(((\\w*)?/?)*)': {
+      transform: '@heroicons/react/24/outline/{{ matches.[1] }}/{{member}}',
+    },
+  },
+}
 
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
