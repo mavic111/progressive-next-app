354b6e1289fabfeee122d2bb15b4df96a07c26c4
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-06-18 08:16:12 +0700

    Fix shadow

--- components/organisms/ScrollView.tsx
+++ components/organisms/ScrollView.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export default function ScrollView({ children }: { children: ReactNode }) {
   return (
-    <section className="flex h-full w-full flex-col overflow-y-scroll md:rounded-xl md:bg-slate-100 md:shadow-inner md:dark:bg-zinc-900">
+    <section className="flex h-full w-full flex-col overflow-y-scroll shadow-inner md:rounded-xl md:bg-slate-100 md:dark:bg-zinc-900">
       {children}
     </section>
   )
