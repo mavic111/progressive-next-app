a70a55aa921511280c86962cd8579d28781edbbd
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-06-24 03:08:31 +0700

    [UI] Eager loading logo to reduce LCP

--- components/atoms/PageTitle.tsx
+++ components/atoms/PageTitle.tsx
@@ -4,6 +4,7 @@ export default function PageTitle() {
   return (
     <div className="relative h-6 w-12 md:h-8 md:w-16">
       <Image
+        loading="eager"
         style={{ objectFit: 'contain' }}
         className="dark:invert"
         fill
