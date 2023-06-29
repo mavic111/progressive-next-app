d014fd3554dd10ad43afa7f83f6924bc5b9e5804
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-06-30 01:20:51 +0700

    Remove header shadow

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -3,7 +3,7 @@ import Toolbar from './Toolbar'
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-20 flex w-full items-center justify-between bg-white p-4 shadow dark:bg-zinc-950 dark:shadow-zinc-900/50 md:bg-transparent md:shadow-none md:dark:bg-transparent">
+    <header className="sticky top-0 z-20 flex w-full items-center justify-between bg-white p-4 dark:bg-zinc-950 md:bg-transparent md:dark:bg-transparent">
       <PageTitle />
       <Toolbar />
     </header>
