171df9bad05c29842fd500d1509391f16676030f
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-06-30 01:25:53 +0700

    Remove border

--- components/organisms/ScrollView.tsx
+++ components/organisms/ScrollView.tsx
@@ -4,7 +4,7 @@ export default function ScrollView({ children }: { children: ReactNode }) {
   return (
     <section
       id="scroll-view"
-      className="no-scrollbar flex h-full w-full flex-col overflow-y-scroll border border-dashed border-red-500/50"
+      className="no-scrollbar flex h-full w-full flex-col overflow-y-scroll"
     >
       {children}
     </section>
