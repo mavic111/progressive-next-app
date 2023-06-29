8fd390951a0fd04b801284c875a29978be14afcc
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-06-30 00:16:38 +0700

    Add border into scroll view

--- components/organisms/ScrollView.tsx
+++ components/organisms/ScrollView.tsx
@@ -2,7 +2,10 @@ import { ReactNode } from 'react'
 
 export default function ScrollView({ children }: { children: ReactNode }) {
   return (
-    <section className="no-scrollbar flex h-full w-full flex-col overflow-y-auto">
+    <section
+      id="scroll-view"
+      className="no-scrollbar flex h-full w-full flex-col overflow-y-scroll border border-dashed border-red-500/50"
+    >
       {children}
     </section>
   )
