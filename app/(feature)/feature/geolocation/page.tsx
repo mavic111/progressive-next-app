7206c2c5f9af1a3e39fe0783da9a0df5b77d023d
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-07-02 16:18:26 +0700

    Fix section padding

--- app/(feature)/feature/geolocation/page.tsx
+++ app/(feature)/feature/geolocation/page.tsx
@@ -1,4 +1,5 @@
 import MyLocation from '@/components/molecules/MyLocation'
+import ScrollView from '@/components/organisms/ScrollView'
 import Section from '@/components/organisms/Section'
 import { Button } from '@/components/ui/button'
 import { Card, CardFooter, CardHeader } from '@/components/ui/card'
@@ -6,39 +7,41 @@ import Link from 'next/link'
 
 export default function Geolocation() {
   return (
-    <Section className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6">
-      <Card className="flex h-max flex-col justify-between rounded-2xl bg-violet-200 dark:bg-violet-800">
-        <CardHeader>
-          <h1>Geolocation</h1>
-          <p>
-            The Geolocation API is used to retrieve the user&apos;s location, so
-            that it can for example be used to display their position using a
-            mapping API.
-          </p>
-        </CardHeader>
-        <CardFooter>
-          <div className="flex flex-col space-y-4">
-            <Link
-              href={
-                'https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API'
-              }
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button variant={'default'}>Documentation</Button>
-            </Link>
-            <Link
-              href={'https://caniuse.com/#feat=geolocation'}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button variant={'default'}>Browser support</Button>
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+    <ScrollView>
+      <Section className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 md:gap-6">
+        <Card className="flex h-max flex-col justify-between rounded-2xl bg-violet-200 dark:bg-violet-800">
+          <CardHeader>
+            <h1>Geolocation</h1>
+            <p>
+              The Geolocation API is used to retrieve the user&apos;s location,
+              so that it can for example be used to display their position using
+              a mapping API.
+            </p>
+          </CardHeader>
+          <CardFooter>
+            <div className="flex flex-col space-y-4">
+              <Link
+                href={
+                  'https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API'
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant={'default'}>Documentation</Button>
+              </Link>
+              <Link
+                href={'https://caniuse.com/#feat=geolocation'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant={'default'}>Browser support</Button>
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
 
-      <MyLocation />
-    </Section>
+        <MyLocation />
+      </Section>
+    </ScrollView>
   )
 }
