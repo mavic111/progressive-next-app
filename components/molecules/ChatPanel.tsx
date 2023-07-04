5a9d337650cbd53f40f56796681fb9ed16773d17
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-07-04 10:26:58 +0700

    Redesign ChatPanel

--- components/molecules/ChatPanel.tsx
+++ components/molecules/ChatPanel.tsx
@@ -7,14 +7,14 @@ import { Bot, User } from 'lucide-react'
 export default function ChatPanel() {
   const { messages, input, handleInputChange, handleSubmit } = useChat()
   return (
-    <div className="mx-auto flex h-full w-full max-w-2xl flex-col justify-between">
+    <div className="mx-auto flex h-full w-full max-w-3xl flex-col justify-between lg:max-w-5xl">
       <div className="no-scrollbar h-full space-y-4 overflow-y-scroll py-4">
         {messages.length > 0 ? (
           messages.map((m) => (
             <ChatBubble key={m.id} role={m.role} content={m.content} />
           ))
         ) : (
-          <span className="text-xl font-semibold md:text-3xl">
+          <span className="flex text-xl font-semibold md:text-3xl">
             Welcome to AI Chatbot!
           </span>
         )}
@@ -24,7 +24,7 @@ export default function ChatPanel() {
         <label className="flex space-x-4">
           <input
             placeholder="Say something..."
-            className="w-full rounded border border-gray-300 p-2 shadow-xl"
+            className="w-full rounded border border-gray-300 p-2"
             value={input}
             onChange={handleInputChange}
           />
@@ -39,11 +39,11 @@ function ChatBubble({ role, content }: { role: string; content: string }) {
   return (
     <div className="flex gap-2">
       {role === 'user' ? (
-        <User className="h-6 w-6 shrink-0" />
+        <User className="h-6 w-6 shrink-0 md:h-8 md:w-8" />
       ) : (
-        <Bot className="h-6 w-6 shrink-0" />
+        <Bot className="h-6 w-6 shrink-0 md:h-8 md:w-8" />
       )}
-      {content}
+      <div className="mt-0.5 md:text-lg">{content}</div>
     </div>
   )
 }
