2439a49a67b76510b509683e2019c08aad984592
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-07-05 16:28:41 +0700

    Override default runtimeCaching, with next-data cache strategy: NetworkFirst

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require('./pwa/runtimeCaching')
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
+  extendDefaultRuntimeCaching: runtimeCaching,
 })
 
 module.exports = withPWA(nextConfig)
