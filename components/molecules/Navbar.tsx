739fde94316bfe3c2424dc3cbd02ed7a7db26c55
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-07-06 07:02:14 +0700

    Navbar scroll when overflow

--- components/molecules/Navbar.tsx
+++ components/molecules/Navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
   })
 
   return (
-    <nav className="flex w-full min-w-min max-md:fixed max-md:bottom-0 max-md:z-20 max-md:justify-center md:z-0 md:max-w-max md:grow md:flex-col lg:mx-4">
+    <nav className="md:no-scrollbar flex w-full min-w-min max-md:fixed max-md:bottom-0 max-md:z-20 max-md:justify-center md:z-0 md:max-w-max md:grow md:flex-col md:overflow-y-auto lg:mx-4">
       <ol className="grid h-full w-full max-w-sm grid-cols-4 gap-2 rounded-3xl p-4 md:h-fit md:grid-cols-1 md:flex-col md:justify-start md:gap-4 md:rounded-none md:bg-transparent md:p-0 md:shadow-none md:dark:bg-transparent">
         {NavLinks}
       </ol>
